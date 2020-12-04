f6423680843151fddeac6ea796fe30a2579bcc1b
ibosnic <igorbosnic01@gmail.com>
2020-12-03 21:30:24 -0500

    Removed old project and created core project.

--- SortableAuction/AuctionConsoleApp/Program.cs
+++ SortableAuction/AuctionConsoleApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AuctionConsoleApp
 {
@@ -10,7 +6,7 @@ namespace AuctionConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
